929710bb4e8a7d3295844cb192f6284858c49a57
kMercu <141454203+kMercu@users.noreply.github.com>
2023-08-22 22:49:48 -0500

    Update AñoBisiesto.java

--- AñoBisiesto.java
+++ AñoBisiesto.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 public class AñoBisiesto {
     public static void main(String[] args) {
-        // Crear un objeto Scanner para leer la entrada del usuario
-        Scanner scanner = new Scanner(System.in);
+        // En esta parte creamos un objeto Scanner para leer la entrada del usuario
+        Scanner sc = new Scanner(System.in);
         
-        // Solicitar al usuario que ingrese un año
+        // Solicitamos los datos al usuario para que ingrese un año
         System.out.print("Ingrese un año: ");
-        int año = scanner.nextInt();
+        int año = sc.nextInt();
         
-        // Verificar si el año es bisiesto
+        // Verificamos si el año es bisiesto
         boolean esBisiesto = esBisiesto(año);
         
-        // Mostrar el resultado
+        // Mostramos el resultado
         if (esBisiesto) {
             System.out.println(año + " es un año bisiesto.");
         } else {
             System.out.println(año + " no es un año bisiesto.");
         }
         
-        // Cerrar el objeto Scanner
-        scanner.close();
-    }
+        // Cerramos el objeto Scanner
+        sc.close();
+    }//Cierra main
     
-    // Método para verificar si un año es bisiesto
+    // Con este metodo verificamos si un año es bisiesto
     public static boolean esBisiesto(int anio) {
         if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
             return true;
         } else {
             return false;
         }
-    }
-}
+    }//Cierra esBisiesto
+}//Cierra class
